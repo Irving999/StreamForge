7e4382be6b8e3c42760aedeb13cae12e9de6dd90
Irving Bautista <irvingbautista681@gmail.com>
2026-09-08 11:57:32 -0700

    Accept video uploads

--- server.py
+++ server.py
@@ -1,7 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
 
 app = FastAPI()
-
-@app.get("/")
-def root():
-    return {"message": "Hello World!"}
+    
+@app.post("/videos/")
+async def upload_video(file: UploadFile = File(...)):
+    return {
+        "filename": file.filename,
+        "content-type": file.content_type,
+        "size": file.size
+    }

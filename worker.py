b66af5a176281b4bae44be558645afb2f2ee1a68
Irving Bautista <irvingbautista681@gmail.com>
2026-09-08 18:51:11 -0700

    Update job status when processing starts

--- worker.py
+++ worker.py
@@ -1,4 +1,5 @@
 import redis
+from database import get_db_connection
 
 client = redis.Redis(
     host="localhost",
@@ -12,4 +13,24 @@ print("Worker is waiting for job...")
 
 while True:
     _, job_id = client.blpop("processing_queue")
-    print(f"Received job:{job_id}")
+
+    conn = get_db_connection()
+
+    cur = conn.cursor()
+
+    cur.execute(
+        """
+        UPDATE jobs
+        SET status = 'processing',
+            started_at = NOW()
+        WHERE id = %s
+        """,
+        (job_id,),
+    )
+
+    conn.commit()
+
+    cur.close()
+    conn.close()
+    
+    print(f"Processing job:{job_id}")

e2297d33b31548458948a33ef23c1342526a5b5a
Irving Bautista <irvingbautista681@gmail.com>
2026-09-15 12:54:28 -0700

    Publish Docker images to ECR from GitHub Actions

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -104,6 +104,7 @@ resource "aws_ecs_task_definition" "api" {
         secretOptions = []
 
         options = {
+          "awslogs-create-group"  = "true"
           "awslogs-group"         = aws_cloudwatch_log_group.api.name
           "awslogs-region"        = var.aws_region
           "awslogs-stream-prefix" = "ecs"
@@ -193,6 +194,7 @@ resource "aws_ecs_task_definition" "worker" {
         secretOptions = []
 
         options = {
+          "awslogs-create-group"  = "true"
           "awslogs-group"         = aws_cloudwatch_log_group.worker.name
           "awslogs-region"        = var.aws_region
           "awslogs-stream-prefix" = "ecs"
---
+++ terraform/github_oidc.tf
@@ -0,0 +1,70 @@
+resource "aws_iam_openid_connect_provider" "github_actions" {
+  url = "https://token.actions.githubusercontent.com"
+
+  client_id_list = [
+    "sts.amazonaws.com"
+  ]
+}
+
+resource "aws_iam_role" "github_deploy" {
+  name = "streamforge-github-deploy"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Effect = "Allow"
+
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.github_actions.arn
+        }
+
+        Action = "sts:AssumeRoleWithWebIdentity"
+
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+            "token.actions.githubusercontent.com:sub" = "repo:Irving999/StreamForge:ref:refs/heads/main"
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "github_deploy_ecr" {
+  name = "StreamForgeGitHubECR"
+  role = aws_iam_role.github_deploy.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Effect = "Allow"
+
+        Action = [
+          "ecr:GetAuthorizationToken"
+        ]
+
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+
+        Action = [
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchGetImage",
+          "ecr:CompleteLayerUpload",
+          "ecr:InitiateLayerUpload",
+          "ecr:PutImage",
+          "ecr:UploadLayerPart"
+        ]
+
+        Resource = aws_ecr_repository.streamforge.arn
+      }
+    ]
+  })
+}
